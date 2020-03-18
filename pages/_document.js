39e67f0b9dfe7d1ea7624bb3d413b063dc5b62b0
Phuong <phuong.da@appfast.io>
2020-03-18 22:39:45 +0700

    fix bug

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
+    let fullUrl = "";
     if (req) {
       fullUrl = req.protocol + "://" + req.get("host") + req.originalUrl;
     } else {
