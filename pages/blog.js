2092cf3a5a341567fbd6a71bdf1410c550bc02a1
Phuong <phuong.da@appfast.io>
2020-03-18 21:52:27 +0700

    push

---
+++ pages/blog.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Head from "next/head";
+
+const Blog = () => (
+  <div>
+    <Head></Head>
+    <div className="hero">BLOG</div>
+  </div>
+);
+
+export default Blog;
